e7f7ffe789a5cc019492010db85a65b6277b1db4
ubertrombone <josh_rose@alumni.baylor.edu>
2025-05-02 18:58:25 +0300

    Changes dependency order to fix import issue.

--- windowKit/build.gradle.kts
+++ windowKit/build.gradle.kts
@@ -34,9 +34,9 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
+            implementation(compose.components.resources)
             implementation(compose.material3)
             implementation(compose.ui)
-            implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.aakira.napier)
             implementation(libs.kotlinx.collections)
