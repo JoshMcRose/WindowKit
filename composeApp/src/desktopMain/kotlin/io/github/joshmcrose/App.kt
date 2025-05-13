a61290168e05b653ef24fb3b13c36b50797b5cfa
joshrose <josh_rose@alumni.baylor.edu>
2025-05-12 21:03:36 -0500

    Updates app background color.

--- composeApp/src/desktopMain/kotlin/io/github/joshmcrose/App.kt
+++ composeApp/src/desktopMain/kotlin/io/github/joshmcrose/App.kt
@@ -1,14 +1,10 @@
 package io.github.joshmcrose
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import io.github.joshmcrose.theme.WindowTheme
 
 @Composable
 fun App() {
